782a924027645076c82468b2874757e28c470d66
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-11-20 20:05:46 +0700

    memambahkan efek hover pada navbar, memperbaiki tata letak pada main, dan menambahkan ikon sosial media pada footer

--- web-fidealify/src/script/component/foot-bar.js
+++ web-fidealify/src/script/component/foot-bar.js
@@ -6,34 +6,34 @@ class FootBar extends HTMLElement {
     render() {
         this.innerHTML = `
         <hr>
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-4 offset-sm-1">
-                        <h4 class="title">Fidealify</h4>
-                        <p>Satu dari rumahmu untuk mengukur kesehatan lemakmu.</p>
-                        <ul class="social-icon">
-                            <a href="#" class="social"><i class="fa fa-twitter" aria-hidden="true"></i>Twitter</a>
-                            <a href="#" class="social"><i class="fa fa-instagram" aria-hidden="true"></i>Instagram</a>
-                            <a href="#" class="social"><i class="fa fa-youtube-play" aria-hidden="true"></i>YouTube</a>
-                        </ul>
-                    </div>
-                    <div class="col-sm-5 offset-sm-2">
-                        <ul>
-                            <li><a href="#"><i class="fa fa-cc-amex" aria-hidden="true"></i>Beranda</a></li>
-                            <li><a href="#"><i class="fa fa-credit-card" aria-hidden="true"></i>Timbang</a></li>            
-                            <li><a href="#"><i class="fa fa-paypal" aria-hidden="true"></i>Blog</a></li>
-                            <li><a href="#"><i class="fa fa-cc-visa" aria-hidden="true"></i>Tentang Kami</a></li>
-                        </ul>
-                    </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 offset-sm-1">
+                    <h4 class="title">Fidealify</h4>
+                    <p>Satu dari rumahmu untuk mengukur kesehatan lemakmu.</p>
+                    <ul class="social-icon">
+                        <a href="#" class="social"><i class="fa-brands fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa-brands fa-instagram" aria-hidden="true"></i></i></a>
+                        <a href="#" class="social"><i class="fa-brands fa-youtube" aria-hidden="true"></i></a>
+                    </ul>
                 </div>
+                <div class="nav-bottom-custom col-sm-5 offset-sm-2">
+                    <ul>
+                        <li><a href="#">Beranda</a></li>
+                        <li><a href="#">Timbang</a></li>            
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Tentang Kami</a></li>
+                    </ul>
+                </div>
+            </div>
                 
                 
-                <div class="row rule">
-                    <div class="col-sm-4 offset-sm-1">
+                <div class="rule-custom row">
+                    <div class="copyright-custom col-sm-4 offset-sm-1">
                         <p>Copyright © 2022 Fidealify. All rights reserved.</p>
                     </div>
-                    <div class="col-sm-5 offset-sm-2 policy">
+                    <div class="policy-custom col-sm-5 offset-sm-2">
                         <a href="#">Terms & Conditions</a>
                         <a href="#">Privacy Policy</a>            
                         <a href="#">Cookie Policy</a>
@@ -42,7 +42,7 @@ class FootBar extends HTMLElement {
                 
                 
             
-        </footer>
+
         `;
     }
 }
--- web-fidealify/src/script/component/head-bar.js
+++ web-fidealify/src/script/component/head-bar.js
@@ -7,25 +7,25 @@ class HeadBar extends HTMLElement {
    
     render() {
       this.innerHTML = `
-        <nav class="navbar navbar-expand-lg bg-light">
+        <nav class="navbar navbar-custom navbar-expand-lg bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Fi</a>
+                <a class="navbar-brand navbar-brand-custom" href="#">Fi</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                        <li class="nav-item nav-item-custom">
+                            <a class="nav-link nav-link-custom active" aria-current="page" href="#">Beranda</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Timbang</a>
+                        <li class="nav-item nav-item-custom">
+                            <a class="nav-link nav-link-custom" href="#">Timbang</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Blog</a>
+                        <li class="nav-item nav-item-custom">
+                            <a class="nav-link nav-link-custom" href="#">Blog</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Tentang</a>
+                        <li class="nav-item nav-item-custom">
+                            <a class="nav-link nav-link-custom" href="#">Tentang</a>
                         </li>
                     </ul>
                 </div>
