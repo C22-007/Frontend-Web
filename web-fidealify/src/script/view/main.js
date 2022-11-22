3be4bcfa2c97b3baa958b33752b193682c893585
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-11-22 22:52:25 +0700

    membuat animasi range kategori

---
+++ web-fidealify/src/script/view/main.js
@@ -0,0 +1,109 @@
+let barCircular = document.querySelector('.bar-circular');
+let percentage = document.querySelector('.bar-percentage');
+// let inputBar = document.querySelector('.bar-input');
+// let buttonBar = document.querySelector('.bar-button');
+let start = 1;
+
+let inputUsia = document.getElementById('inputUsia');
+let inputTinggi = document.querySelector('#inputTinggi');
+let inputBerat = document.querySelector('#inputBerat');
+let buttonHasil = document.querySelector('#buttonHasil');
+let buttonReset = document.querySelector('#buttonReset');
+
+const category = document.querySelector('.image-custom');
+
+
+function displayProgress() {
+    percentage.innerText = `${calculateBodyfat()}%`;
+    barCircular.style.background = `conic-gradient(#3F6FF5 ${calculateBodyfat() * 3.6}deg, #ededed 0deg)`;   
+
+    return percentage.innerText;
+}
+
+
+
+function calculateBodyfat() {
+    let result = 0;
+    // result = inputUsia.value;
+    result = parseInt(inputUsia.value) + parseInt(inputTinggi.value) + parseInt(inputBerat.value);
+    
+    return result;
+}
+
+function displayCategory() {
+    
+    if(calculateBodyfat() > 0 && calculateBodyfat() < 50){
+        category.setAttribute("src", "./assets/images/bodyfat-male-average.jpg");
+    }
+    // console.log(calculateBodyfat());
+}
+
+// console.log(displayProgress());
+
+// console.log(typeof(inputUsia.value));
+buttonHasil.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    displayProgress();
+    displayCategory();
+});
+
+
+
+// console.log(calculateBodyfat());
+
+// export {displayProgress};
+// export default displayProgress;
+
+// buttonHasil.addEventListener('click', (event) => { 
+//     event.preventDefault();
+
+//     let result = 0;   
+//     result = parseInt(inputUsia.value) + parseInt(inputTinggi.value) + parseInt(inputBerat.value);
+
+//     for(let i = 0; i <= result; i++){
+//         percentage.innerText = `${i}%`;
+//         barCircular.style.background = `conic-gradient(#3F6FF5 ${i * 3.6}deg, #ededed 0deg)`;   
+//     }
+    
+//     const progress = setInterval(() => {
+//         if(result > start) {
+//             start++;
+//             displayProgress();
+            
+//         } else {
+//             start--;
+//             displayProgress();
+//         }
+        
+        
+//         function displayProgress() {
+//             percentage.innerText = `${start}%`;
+//             barCircular.style.background = `conic-gradient(#3F6FF5 ${start * 3.6}deg, #ededed 0deg)`;   
+//             if(result == start) {
+//                 clearInterval(progress);
+//                 result = 0;
+//             }
+//             return percentage.innerText;
+//         }
+        
+//     });
+
+//     console.log(progress);
+// });
+
+
+
+buttonReset.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    inputUsia.value = '';
+    inputTinggi.value = '';
+    inputBerat.value = '';
+    percentage.textContent = '0%';
+    barCircular.style.background = `conic-gradient(#3F6FF5 0deg, #ededed 0deg)`;
+    category.setAttribute("src", "");
+    // console.log('halo');
+});
+
+
