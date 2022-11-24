3bdafcdfeb1a450278089ccba874c0430578cdbd
Rachma Yuni Andari <rachmayuniandari@gmail.com>
2022-11-24 08:33:52 +0700

    edited container kolom input timbang
    sebelah kanan untuk result belum

--- 4-page-Fidealify/4. input/js-input.js
+++ 4-page-Fidealify/4. input/js-input.js
@@ -44,6 +44,35 @@ function myFunction() {
     }
 }
 
+// INPUT GENDER SELECT
+$("select").on("click" , function() {
+  
+    $(this).parent(".select-box").toggleClass("open");
+    
+  });
+  
+  $(document).mouseup(function (e)
+  {
+      var container = $(".select-box");
+  
+      if (container.has(e.target).length === 0)
+      {
+          container.removeClass("open");
+      }
+  });
+  
+  
+  $("select").on("change" , function() {
+    
+    var selection = $(this).find("option:selected").text(),
+        labelFor = $(this).attr("id"),
+        label = $("[for='" + labelFor + "']");
+      
+    label.find(".label-desc").html(selection);
+      
+  });
+
+// AKHIR INPUT GENDER SELECT
 
 /* TESTIMONIAL / WRAPPER / CAROUSEL */
 
