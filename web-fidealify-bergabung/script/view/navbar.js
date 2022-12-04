57486367ce39f914ea418bf3fcbcb94b558b4475
AriestaTegar-JR <ariestategarjr87@gmail.com>
2022-12-04 13:49:46 +0700

    ulang navbar fixed

--- web-fidealify-bergabung/script/view/navbar.js
+++ web-fidealify-bergabung/script/view/navbar.js
@@ -2,15 +2,30 @@ const navbarToggler = document.querySelector('.navbar-toggler');
 const mainArea = document.querySelector('main');
 
 
-// const navbarAttributes = navbarToggler.getAttributeNames();
+
+// const navbarAttributes = navbarToggler.getAttributeNames('class');
 // const navbarAttributes = navbarToggler.getAttribute('aria-expanded');
-const navbarAttributes = navbarToggler.getAttribute('aria-expanded');
+// const navbarAttributes = navbarToggler.getAttribute('class');
 
 
-
+let counter = 0;
 navbarToggler.addEventListener('click', () => {
-    console.log(navbarAttributes);
+    // console.log(navbarAttributes);
     // console.log(navbarToggler.classList[1]);
+    
+    // console.log(counter++);
+    
+    if(counter == 1 && navbarToggler.classList[1] == undefined) {
+        // console.log('buka');
+        mainArea.style.marginTop = "6rem";
+    }
+
+
+    if(navbarToggler.classList[1] == undefined && counter != 1) {
+        mainArea.style.marginTop = "6rem";
+    } else if(navbarToggler.classList[1] == 'collapsed') {
+        mainArea.style.marginTop = "0";
+    }
     // console.log(mainArea);
     // if(mainArea.style.marginTop == "6rem") {
     //     mainArea.style.marginTop = "6rem";
